e513059ae7cf855e2cdea59f91799edba1772244
Y0L042 <66077762+Y0L042@users.noreply.github.com>
2022-10-12 13:30:39 +0200

    QS

--- UI/ui_main_menu.gd
+++ UI/ui_main_menu.gd
@@ -10,11 +10,18 @@ func _on_start_new_game_pressed() -> void:
 func _on_quit_pressed() -> void:
 	quit()
 
+func _on_continue_game_pressed() -> void:
+	pass # Replace with function body.
 
 
 func start_new_game():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	get_parent().queue_free()
 	get_tree().change_scene_to_packed(SceneLib.OUTDOOR_WORLD)
+
 
 func quit():
 	get_tree().quit()
+
+
+
