5be0bb00ac8e4799b87fcf5ea55832da9cdcd427
Y0L042 <66077762+Y0L042@users.noreply.github.com>
2022-10-12 13:30:39 +0200

    QS

--- UI/ui_pause_menu.gd
+++ UI/ui_pause_menu.gd
@@ -1,12 +1,14 @@
 extends Control
 
-
+@onready var canvas_layer = %CanvasLayer
 
 func _ready() -> void:
-	pause()
+	canvas_layer.set_visible(false)
 
 func _on_main_menu_pressed() -> void:
+	get_parent().queue_free()
 	get_tree().change_scene_to_packed(SceneLib.MAIN_MENU)
+
 
 func _on_resume_pressed() -> void:
 	unpause()
@@ -17,10 +19,12 @@ func _on_quit_pressed() -> void:
 
 
 func pause():
+	canvas_layer.set_visible(true)
 	get_tree().paused = true
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
 func unpause():
+	canvas_layer.set_visible(false)
 	get_tree().paused = false
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
