20393705eb1972c38c84734f1aa4dc0c5c50cbef
Y0L042 <66077762+Y0L042@users.noreply.github.com>
2022-10-25 16:03:12 +0200

    QS busy with poisson
    https://youtu.be/flQgnCUxHlw?t=1977

--- Procedural Map Worlds/Perlin World/noise_world_generator.gd
+++ Procedural Map Worlds/Perlin World/noise_world_generator.gd
@@ -89,8 +89,8 @@ func clean_map(new_tilemap: TileMap = tilemap, new_size: Vector2 = size):
 func populate_map_blueprint():
 	pass
 
-class CustomArray:
-	pass
+
+
 
 func poisson_disc_sampler(new_tilemap: TileMap = tilemap):
 	var new_size = tilemap.get_used_rect().end
@@ -109,15 +109,44 @@ func poisson_disc_sampler(new_tilemap: TileMap = tilemap):
 	# Step 1: Sample random point
 	var x: int = randi_range(-new_size.x, new_size.x)
 	var y: int = randi_range(-new_size.y, new_size.y)
-	var i = floor(x / width)
-	var j = floor(y/ width)
-	var pos := Vector2i(x, y)
-	grid[i + j * cols] = pos # add sample position to grid???
+	var ii = floor(x / width)
+	var jj = floor(y/ width)
+	var pos := Vector2(x, y)
+	grid[ii + jj * cols] = pos # add sample position to grid???
 	active_points.append(pos) # add sample position to list of samples
 
+	# Step 2:
+#	while !active_points.is_empty():
+	for run in 5000:
+		var rand_index: int = randi() % active_points.size()
+		var rand_point = active_points[rand_index]
+		var found: bool = false
+		for n in k:
+			# Step 2.a
+			var active_sample = Vector2(randi_range(-360, 360), randi_range(-360, 360)).normalized()
+			var rand_length = randi_range(radius, 2 * radius)
+			active_sample *= rand_length
+			active_sample += rand_point
+			var col = floor(active_sample.x / width)
+			var row = floor(active_sample.y / width)
+			if (col < 0 or col > cols) and (row < 0 or row > rows):
+				continue
 
-
-
+			# Step 2.b
+			var ok: bool = true
+			for i in range(-1, 1):
+				for j in range(-1, 1):
+					var neighbor = grid[col+i + (row+j) * cols]
+					if typeof(neighbor) == TYPE_VECTOR2:
+						var dist = active_sample.distance_to(neighbor)
+						if dist < radius:
+							ok = false
+			if ok:
+				found = true
+				grid[col + row * cols] = active_sample
+				active_points.append(active_sample)
+		if !found:
+			active_points.erase(rand_index)
 
 
 
