289111c8b03fc26b42816b2ac96d857b3185c774
Y0L042 <66077762+Y0L042@users.noreply.github.com>
2022-10-25 14:38:43 +0200

    QS

--- Procedural Map Worlds/Perlin World/noise_world_generator.gd
+++ Procedural Map Worlds/Perlin World/noise_world_generator.gd
@@ -52,6 +52,8 @@ func generate_map_blueprint(new_tilemap: TileMap = tilemap, new_size: Vector2 = 
 			var tile = get_tile_index(noise.get_noise_2d(float(x), float(y)))
 			new_tilemap.set_cell(0, Vector2i(x, y), 1, tile)
 
+	poisson_disc_sampler()
+
 	return new_tilemap
 
 
@@ -91,45 +93,31 @@ class CustomArray:
 	pass
 
 func poisson_disc_sampler(new_tilemap: TileMap = tilemap):
-	var new_size = tilemap.get_used_rect()
+	var new_size = tilemap.get_used_rect().end
 	var radius: int = 10 # radius of disks
 	var k: int = 30 # sample limit before rejection
 	var grid: Array
 	var width: float = radius / sqrt(2)
 	var cols = floor(new_size.x / width)
 	var rows = floor(new_size.y / width)
-
-	var sample_points: Array = [[],[]]
-
-
+	var active_points: Array
 
 	# Step 0: Set sample_points array to 0 (it also sets size)
-	for x in new_size.x:
-		sample_points.append([])
-		for y in new_size.y:
-			sample_points[x].append(0)
+	for index in cols*rows:
+		grid.append(0)
 
 	# Step 1: Sample random point
-	var tempbool := true
-	while tempbool:
-		var x: int = randi_range(-new_size.x, new_size.x)
-		var y: int = randi_range(-new_size.y, new_size.y)
-		var pos := Vector2i(x, y)
-		if new_tilemap.get_cell_atlas_coords(0, pos) == SceneLib.TILES.WHITE:
-			sample_points[x][y] = 1
-			var active_sample = sample_points[x][y]
-			tempbool = false
+	var x: int = randi_range(-new_size.x, new_size.x)
+	var y: int = randi_range(-new_size.y, new_size.y)
+	var i = floor(x / width)
+	var j = floor(y/ width)
+	var pos := Vector2i(x, y)
+	grid[i + j * cols] = pos # add sample position to grid???
+	active_points.append(pos) # add sample position to list of samples
 
-	# Step 2: Get a random sample in a range of focus point
-	while !sample_points.is_empty():
-		var x: int = randi_range(-new_size.x, new_size.x)
-		var y: int = randi_range(-new_size.y, new_size.y)
-		var active_point := Vector2i(x, y)
-		var k: int = 5 #?
-		for index in k:
-			var rand_vec = Vector2(randf_range(-360, 360), randf_range(-360, 360)).normalized()
-			rand_vec *= randi_range(radius, 3*radius)
-			rand_vec = rand_vec.round()
+
+
+
 
 
 
