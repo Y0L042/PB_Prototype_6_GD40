0d13f8377b0f679c1f1fba2b21ca5e1eceeedd36
Y0L042 <66077762+Y0L042@users.noreply.github.com>
2022-11-01 14:17:20 +0200

    QS

--- main.gd
+++ main.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 class_name Main
 
+var player_party_manager: PlayerPartyManager
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -12,24 +14,37 @@ func _process(delta: float) -> void:
 	pass
 
 
-func change_level(old_level, new_level, parent):
-	old_level.queue_free()
-	SceneLib.spawn_child(new_level, self)
 
 
-func _on_ui_main_menu_start_new_game(main_menu) -> void:
-	var new_level = SceneLib.STARTING_MAP
-	change_level(main_menu, new_level, self)
-	instantiate_player()
 
 #-------------------------------------------------------------------------------
 # Initialisation
 #-------------------------------------------------------------------------------
-func instantiate_player():
-	SceneLib.spawn_child(SceneLib.PLAYER_PARTY, self)
+func _on_ui_main_menu_start_new_game(main_menu) -> void:
+	var first_level = SceneLib.STARTING_MAP
+	change_level(main_menu, first_level, self)
+#	var pause_time: float = 2.5
+#	await get_tree().create_timer(pause_time).timeout
+	spawn_player_manager()
+
+func spawn_player_manager():
+	player_party_manager = SceneLib.spawn_child(SceneLib.PLAYER_PARTY, self)
+	player_party_manager.spawn_actors()
 	# set player position
 	# spawn soldiers
 
+
+#-------------------------------------------------------------------------------
+# Events
+#-------------------------------------------------------------------------------
+func change_level(old_level, new_level, parent):
+	old_level.queue_free()
+	new_level = SceneLib.spawn_child(new_level, self)
+
+
+#-------------------------------------------------------------------------------
+# Runtime
+#-------------------------------------------------------------------------------
 
 #-------------------------------------------------------------------------------
 # UI Interaction
