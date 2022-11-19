188d379cbb9f5779555b832dc220df80921baecf
Y0L042 <66077762+Y0L042@users.noreply.github.com>
2022-11-19 07:42:49 +0200

    Update enemy_party_manager.gd
    updated collision avoidance

--- PartyManager/EnemyParty/enemy_party_manager.gd
+++ PartyManager/EnemyParty/enemy_party_manager.gd
@@ -40,46 +40,53 @@ func wander(delta: float):
 	if true: # TODO: if inside map
 		pos = pb.party_target_pos + vel
 
-	var isRaycastIntersected = !do_line_raycast(self.pb.party_pos, pos, 0x1).is_empty()
-	var new_dir
-	if isRaycastIntersected:
-		new_dir = (pos - pb.party_pos).rotated(deg_to_rad(36))
+	var self_pos: Vector2 = self.pb.party_pos
+	var new_dir: Vector2
+	var offset: float = deg_to_rad(36)
+	var isRaycastIntersected: bool = !do_line_raycast(self_pos, pos, 0x1).is_empty()
+	while isRaycastIntersected:
+		new_dir = (pos - pb.party_pos).rotated(offset)
 		pos = new_dir + pb.party_pos
+		isRaycastIntersected = !do_line_raycast(self_pos, pos, 0x1).is_empty()
+		if isRaycastIntersected:
+			new_dir = (pos - pb.party_pos).rotated(-offset)
+			pos = new_dir + pb.party_pos
+			isRaycastIntersected = !do_line_raycast(self_pos, pos, 0x1).is_empty()
+			if isRaycastIntersected:
+				offset *= 2
 
-#	pos = collision_avoidance(pos, delta)
-#	move_formation(pb.party_target_pos, 0)
 	return pos
 
 
-func collision_avoidance(target, self_pos, delta):
-	var RAY_LENGTH: int = GlobalSettings.UNIT * 10
-	var collision_mask: int = 0x1
-	# Calculate direction to target
-	var target_dir = pb.party_pos.direction_to(target)
-
-	# Calculate new position after moving in player direction
-	var new_pos = target
-
-	# do raycast towards target
-	# if it intersects, do one either side, expanding until it finds a clear path
-	var raycast_dir: Vector2 = target_dir
-	var raycast_rotation_offset = deg_to_rad(36)
-	var offset_raycast_dir: Vector2 = raycast_dir
-	var isRaycastIntersected = do_line_raycast(self.pb.party_pos, raycast_dir * RAY_LENGTH, collision_mask).is_empty()
-	while isRaycastIntersected:
-		offset_raycast_dir = raycast_dir.rotated(raycast_rotation_offset)
-		isRaycastIntersected = do_line_raycast(self.pb.party_pos, self.pb.party_pos + offset_raycast_dir * RAY_LENGTH, collision_mask).is_empty()
-		if isRaycastIntersected:
-			offset_raycast_dir = raycast_dir.rotated(-raycast_rotation_offset)
-			isRaycastIntersected = do_line_raycast(self.pb.party_pos, self.pb.party_pos + offset_raycast_dir * RAY_LENGTH, collision_mask).is_empty()
-			if isRaycastIntersected:
-				raycast_rotation_offset *= 2
-			else:
-				break
-		else:
-			break
-	var new_target_pos: Vector2 = pb.party_pos + offset_raycast_dir * party_speed # * delta / 2
-	return new_target_pos
+#func collision_avoidance(target, self_pos, delta):
+#	var RAY_LENGTH: int = GlobalSettings.UNIT * 10
+#	var collision_mask: int = 0x1
+#	# Calculate direction to target
+#	var target_dir = pb.party_pos.direction_to(target)
+#
+#	# Calculate new position after moving in player direction
+#	var new_pos = target
+#
+#	# do raycast towards target
+#	# if it intersects, do one either side, expanding until it finds a clear path
+#	var raycast_dir: Vector2 = target_dir
+#	var raycast_rotation_offset = deg_to_rad(36)
+#	var offset_raycast_dir: Vector2 = raycast_dir
+#	var isRaycastIntersected = do_line_raycast(self.pb.party_pos, raycast_dir * RAY_LENGTH, collision_mask).is_empty()
+#	while isRaycastIntersected:
+#		offset_raycast_dir = raycast_dir.rotated(raycast_rotation_offset)
+#		isRaycastIntersected = do_line_raycast(self.pb.party_pos, self.pb.party_pos + offset_raycast_dir * RAY_LENGTH, collision_mask).is_empty()
+#		if isRaycastIntersected:
+#			offset_raycast_dir = raycast_dir.rotated(-raycast_rotation_offset)
+#			isRaycastIntersected = do_line_raycast(self.pb.party_pos, self.pb.party_pos + offset_raycast_dir * RAY_LENGTH, collision_mask).is_empty()
+#			if isRaycastIntersected:
+#				raycast_rotation_offset *= 2
+#			else:
+#				break
+#		else:
+#			break
+#	var new_target_pos: Vector2 = pb.party_pos + offset_raycast_dir * party_speed # * delta / 2
+#	return new_target_pos
 
 
 func do_line_raycast(from: Vector2, to: Vector2, col_mask = 0xFFFFFFFF):
