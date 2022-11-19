5ccb04258000e892fc986b52eff418afefebc9eb
Y0L042 <66077762+Y0L042@users.noreply.github.com>
2022-11-19 14:25:56 +0200

    Update knight.gd

--- Actor/Knight/knight.gd
+++ Actor/Knight/knight.gd
@@ -8,9 +8,6 @@ var self_scene := SceneLib.KNIGHT
 # States
 #-------------------------------------------------------------------------------
 enum states {
-	PASSIVE,
-	AGGRESSIVE,
-	HURT,
 	DEAD,
 }
 
@@ -22,87 +19,17 @@ func change_state(NEW_STATE: int):
 	current_state = NEW_STATE
 
 func player_state_process():
-	if current_state == states.PASSIVE:
-		state_process_passive()
-	if current_state == states.AGGRESSIVE:
-		state_process_aggressive()
-	if current_state == states.HURT:
-		state_process_hurt()
 	if current_state == states.DEAD:
 		state_process_dead()
 
 func enemy_state_process():
-	if current_state == states.PASSIVE:
-		state_process_passive()
-	if current_state == states.AGGRESSIVE:
-		state_process_aggressive()
-	if current_state == states.HURT:
-		state_process_hurt()
 	if current_state == states.DEAD:
 		state_process_dead()
 
 
 #-------------------------------------------------------------------------------
-# State Functions
+# Player State Functions
 #-------------------------------------------------------------------------------
-func state_process_passive():
-	if pb.isFormationActive:
-		move_target = pb.party_formation.vector_array[actor_formation_index]
-	else:
-		move_target = pb.party_target_pos
-	var stopdist: float = GlobalSettings.UNIT * 0.9
-	var target_dist = get_global_position().distance_to(move_target)
-	var collidingAgainstPersonNextToTarget: bool = false
-	for i in get_slide_collision_count():
-		var collision = get_slide_collision(i).get_collider()
-		if collision and collision.is_in_group(pb.party_group):
-			collidingAgainstPersonNextToTarget = collision.arrivedAtTarget or collidingAgainstPersonNextToTarget
-	var isColliding: bool = get_slide_collision_count() > 0
-	var targetNotMoving: bool = pb.party_target_vel.length() <= 5
-	var withinRadius: bool = target_dist <= pb.active_actors_count*10
-	var nextToTarget: bool = target_dist <= 25
-	arrivedAtTarget = nextToTarget or (targetNotMoving and withinRadius and isColliding and collidingAgainstPersonNextToTarget)
-	if !arrivedAtTarget:
-		var seek_target = SBL.arrive(get_global_position(), move_target, stopdist)
-		steering_vector_array.append(seek_target)
-
-
-
-func state_process_aggressive():
-	## If no enemies is in sight (FOV_enemy_list.is_empty()), go to passive
-	## If there is enemy in sight (!FOV_enemy_list.is_empty()), sort through list to erase dead enemies
-	## If there is alive enemies left over, sort by distance and get the nearest enemy
-	## Move towards enemy
-	## If in weapon range, attack enemy
-	var stopdist: float = GlobalSettings.UNIT * 0.9
-	var target_enemy
-	if FOV_enemy_list.is_empty():
-		change_state(states.PASSIVE)
-	elif !FOV_enemy_list.is_empty():
-		for enemy in FOV_enemy_list:
-			if !enemy.isAlive:
-				FOV_enemy_list.erase(enemy)
-		if !FOV_enemy_list.is_empty():
-			target_enemy = FOV_enemy_list[0]
-			var dist_to_nearest_target: float = self.get_global_position().distance_squared_to(target_enemy.get_global_position())
-			for enemy in FOV_enemy_list:
-				var dist_to_enemy: float = self.get_global_position().distance_squared_to(enemy.get_global_position())
-				if dist_to_enemy < dist_to_nearest_target :
-					target_enemy = enemy
-					dist_to_nearest_target = dist_to_enemy
-			var target_pos: Vector2 = target_enemy.get_global_position()
-			var seek_target = SBL.arrive(get_global_position(), target_pos, stopdist)
-			steering_vector_array.append(seek_target)
-			for weapon in weapon_array:
-				if is_instance_valid(weapon) and dist_to_nearest_target <= (weapon.attack_range*weapon.attack_range):
-					weapon.attack()
-	if !isAlive:
-		change_state(states.DEAD)
-
-
-func state_process_hurt():
-	pass
-
 
 
 func state_process_dead():
