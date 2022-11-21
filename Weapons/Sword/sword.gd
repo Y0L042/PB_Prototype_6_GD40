83023cc7d1c3feb9b421f597fa5839e47e6e49c0
Y0L042 <66077762+Y0L042@users.noreply.github.com>
2022-11-21 13:15:12 +0200

    QS: [BROKEN] Added area to weapon

--- Weapons/Sword/sword.gd
+++ Weapons/Sword/sword.gd
@@ -5,29 +5,35 @@ extends Marker2D
 #-------------------------------------------------------------------------------
 var group: String
 @export var damage: float = 1.0
-@export var attack_range: int = 1 * GlobalSettings.UNIT : set = set_attack_range
+@export var sight_range: float = 5 * GlobalSettings.UNIT : set = set_sight_range
+@export var effective_range: float = 1 * GlobalSettings.UNIT : set = set_effective_range
+@export var trigger_range: float = 1 * GlobalSettings.UNIT : set = set_trigger_range
 
+var enemy_array: Array = []
 #-------------------------------------------------------------------------------
 # Variables
 #-------------------------------------------------------------------------------
 @onready var hitbox: Area2D = %Hitbox
-
-
+@onready var weapon_anim_player: AnimationPlayer = $SwordAnimationPlayer
+@onready var weapon_anim_tree: AnimationTree = %SwordAnimationTree
+@onready var weapon_anim_tree_mode = weapon_anim_tree["parameters/playback"]
+@onready var weapon_area: Area2D = %WeaponArea
 
 
 #-------------------------------------------------------------------------------
 # SetGets
 #-------------------------------------------------------------------------------
-func set_attack_range(new_attack_range):
-	attack_range = new_attack_range * GlobalSettings.UNIT
+func set_sight_range(new_sight_range):
+	sight_range = new_sight_range * GlobalSettings.UNIT
+	weapon_area.shape.set_scale(sight_range)
+
+func set_effective_range(new_effective_range):
+	effective_range = new_effective_range * GlobalSettings.UNIT
+
+func set_trigger_range(new_trigger_range):
+	trigger_range = new_trigger_range * GlobalSettings.UNIT
 
 
-#-------------------------------------------------------------------------------
-# Variables
-#-------------------------------------------------------------------------------
-@onready var weapon_anim_player: AnimationPlayer = $SwordAnimationPlayer
-@onready var weapon_anim_tree: AnimationTree = %SwordAnimationTree
-@onready var weapon_anim_tree_mode = weapon_anim_tree["parameters/playback"]
 
 #-------------------------------------------------------------------------------
 # Initialization
@@ -49,3 +55,12 @@ func _on_hitbox_body_entered(body: Node2D) -> void:
 		if body.pb.party_group != group:
 			body.take_damage(damage)
 
+
+
+func _on_weapon_area_body_entered(body: Node2D) -> void:
+	if body.pb.party_group != group:
+		enemy_array.append(body)
+
+
+func _on_weapon_area_body_exited(body: Node2D) -> void:
+	enemy_array.erase(body)
